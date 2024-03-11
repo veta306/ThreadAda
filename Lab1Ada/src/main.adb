dfc75782e6a2501c79c83d10a0e33f0630ceb18e
veta306 <vv097099@gmail.com>
2024-03-11 12:14:40 +0200

    feat: use entry for tasks

--- Lab1Ada/src/main.adb
+++ Lab1Ada/src/main.adb
@@ -5,7 +5,9 @@ procedure Main is
    pragma Atomic(Can_Stop);
 
    task type Break_Thread;
-   task type Main_Thread(Id: Integer; Step: Long_Long_Integer);
+   task type Main_Thread is
+      entry Start(Id: Integer; Step: Long_Long_Integer);
+   end Main_Thread;
 
    task body Break_Thread is
    begin
@@ -16,20 +18,26 @@ procedure Main is
    task body Main_Thread is
       Sum : Long_Long_Integer := 0;
       Elements : Long_Long_Integer := 0;
+      Task_Id: Integer;
+      Task_Step: Long_Long_Integer;
    begin
+      accept Start (Id :Integer; Step : Long_Long_Integer) do
+         Task_Id := Id;
+         Task_Step := Step;
+      end Start;
       loop
-         Sum := Sum + Elements * Step;
+         Sum := Sum + Elements * Task_Step;
          Elements := Elements + 1;
          exit when Can_Stop;
       end loop;
 
-      Ada.Text_IO.Put_Line("Thread: " & Id'Img & " Sum: " & Sum'Img & " Count: " & Elements'Img);
+      Ada.Text_IO.Put_Line("Thread: " & Task_Id'Img & " Sum: " & Sum'Img & " Count: " & Elements'Img);
    end Main_Thread;
 
    bt1: Break_Thread;
-   Threads : array(1..4) of access Main_Thread;
+   Threads : array(1..4) of Main_Thread;
 begin
    for I in 1..Threads'Length loop
-      Threads(I) := new Main_Thread(I, 1);
+      Threads(I).Start(I, 1);
    end loop;
 end Main;
